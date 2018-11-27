cee66bdc2746e21811dab41ae1e5e98041966572
Michael Kong <wjkonger@gmail.com>
2018-11-27 15:55:24 -0500

    Add connection string

--- CoreAngular/Controllers/SampleDataController.cs
+++ CoreAngular/Controllers/SampleDataController.cs
@@ -1,14 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace CoreAngular.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private string connectionString;
+
+        public SampleDataController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            SqlConnection connection = null;
+
+            connectionString = _configuration.GetConnectionString("appDbConnection");
+
+            //try
+            //{
+            //    connection = new SqlConnection(connectionString);
+
+            //    using (var cmd = new SqlCommand
+            //    {
+            //        Connection = connection,
+            //        CommandText = @"INSERT INTO [dbo].[account] ([username] ,[password]) VALUES (@username, @password)",
+            //        CommandType = CommandType.Text
+            //    })
+            //    {
+            //        connection = null;
+
+            //        cmd.Parameters.AddWithValue("@username", "Shun Shun");
+            //        cmd.Parameters.AddWithValue("@password", "12313");
+
+            //        cmd.Connection.Open();
+            //        cmd.ExecuteNonQuery();
+            //    }
+
+            //}
+            //finally
+            //{
+            //    connection?.Dispose();
+            //}
+        }
+
         private static string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
